32c90cc0c3b2582efcc267b005dd25b7180156f6
Kyle <kyleyoung.9210@gmail.com>
2021-05-11 16:50:47 -0400

    function to add or drop a class on navbar on scroll

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+// declaring variables to add and remove a class on navbar to change background color
+var nav = $('nav');
+var scrollTrigger = 500;
+function getScroll() {
+  return document.body.scrollTop || document.documentElement.scrollTop;
+}
+
+// add class on page load and scroll
+$(window).on('load scroll', function () {
+  if (getScroll() > scrollTrigger) {
+    nav.addClass('navScroll');
+  } else {
+    nav.removeClass('navScroll');
+  }
+});
